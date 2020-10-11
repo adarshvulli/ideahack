a02f73295124356a345a56da06d3da0fdb69df90
kishorkuttan <nandumpilicode@gmail.com>
2020-10-11 18:55:28 +0530

    Final commit

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import os
 from flask import Flask, flash, redirect, render_template, request, session, abort
-from flask_ngrok import run_with_ngrok
 import os
 from flask import Flask, flash, redirect, render_template, request, session, abort
 from keras.models import Sequential
@@ -86,7 +85,6 @@ def search(text):
  
 
 app = Flask(__name__)
-run_with_ngrok(app)  # Start ngrok when app is run
 
 
 @app.route("/")
